faec6021b8b9448012e03140b281de21f111f78e
Roman Dmitriev <fret1995@gmail.com>
2016-12-08 18:31:14 +0300

    v1.1

--- src/Main.py
+++ src/Main.py
@@ -1,24 +1,80 @@
+import os
 import sqlite3
+
+from telebot import types
 
 from cfg.cfg import Config
 from src.SQLite import SQLite
 
-
 import telebot
 import urllib.request
-
 from bs4 import BeautifulSoup
+import sys
+
+sys.path.append("../")
 config = Config()
+
 bot = telebot.TeleBot(config.token)
+
+def get_played_mathes():
+    pass
+
+def get_following_matches():
+    req = urllib.request.Request(
+        config.url,
+        data=None,
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        }
+    )
+    list_matches = []
+    f = urllib.request.urlopen(req)
+    soup = BeautifulSoup(f.read(), "html.parser")
+    matchesItems = BeautifulSoup(str(soup.find_all('div', 'frontpageMatchBox')), "html.parser")
+    # print(matchesItems.prettify())
+    for matchesItem in matchesItems:
+        if matchesItem == "[" or matchesItem == "]" or matchesItem == ", ":
+            pass
+        else:
+            print("-=---------------------")
+            # print(matchesItem)
+            matches = matchesItem.findAll('div')
+            if len(matches) == 7:
+                # for match in matches:
+                q = BeautifulSoup(str(matches[0]), "html.parser")
+                teams = q.findAll('span', {"style": ""})
+                match = [teams[0].string, teams[4].string, "live"]
+                list_matches.append(match)
+                print(teams[0].string, teams[4].string)
+            elif len(matches) == 5:
+                # for match in matches:
+                q = BeautifulSoup(str(matches), "html.parser")
+                teams = q.findAll('span', {"style": ""})
+                time = q.findAll('div', {"style": "font-size: 12px;color:black;"})
+
+                try:
+                    match = [teams[0].string, teams[1].string, time[0].string]
+                    list_matches.append(match)
+                except IndexError:
+                    pass
+    return list_matches
+
+
+
+def get_buttons():
+    keyboard = types.InlineKeyboardMarkup()
+    status = types.InlineKeyboardButton(text='Status', callback_data="status")
+    get_following_matches = types.InlineKeyboardButton(text='Get a list of the following matches', callback_data="get_following_matches")
+    get_played_matches = types.InlineKeyboardButton(text='get a list of matches played', callback_data="get_played_matches")
+    keyboard.add(status)
+    keyboard.add(get_following_matches)
+    keyboard.add(get_played_matches)
+    return keyboard
+
 
 @bot.message_handler(commands=["start"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, 'Чтобы начать получать новости введите /get_news')
-
-
-@bot.message_handler(commands=["status"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, 'Все хорошо. Бот работает.')
 
 
 @bot.message_handler(commands=["get_news"])
@@ -27,11 +83,34 @@ def get_news(message):
     try:
         sql.set_user(message.chat.id)
         bot.send_message(message.chat.id,
-                         'Теперь вы будете получать новости. ')
+                         'Теперь вы будете получать новости. ', reply_markup=get_buttons())
         send_news(message)
     except sqlite3.IntegrityError:
         bot.send_message(message.chat.id,
-                         'Вы уже получаете новости.')
+                         'Вы уже получаете новости.', reply_markup=get_buttons())
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def call_status(call):
+    if call.message:
+        if call.data == "status":
+            bot.send_message(call.message.chat.id, "Все хорошо, все работает.", reply_markup=get_buttons())
+
+        if call.data == "get_following_matches":
+            list_matches = get_following_matches()
+            print(len(list_matches))
+            if list_matches:
+                for l in list_matches:
+                    bot.send_message(call.message.chat.id, l[0] + " vs " + l[1] + "\n\r" + str(l[2]))
+            bot.send_message(call.message.chat.id, "It's all", reply_markup=get_buttons())
+
+        if call.data == "get_played_matches":
+            # list_matches = get_matches()
+            # print(len(list_matches))
+            # if list_matches:
+            #     for l in list_matches:
+            #         bot.send_message(call.message.chat.id, l[0] + " vs " + l[1] + "\n\r" + str(l[2]))
+            bot.send_message(call.message.chat.id, "It's all", reply_markup=get_buttons())
 
 
 @bot.message_handler(commands=["send_news"])
@@ -99,12 +178,12 @@ def set_news(message):
     sql.close()
     print("Запись завершена")
 
+
 if __name__ == '__main__':
     print('Бот запущен')
     config = Config()
     url = config.url
     token = config.token
-    database_name = config.database_name
+    database_name = "../" + config.database_name
 
     bot.polling(none_stop=True)
-
